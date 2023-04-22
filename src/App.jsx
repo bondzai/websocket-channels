f04f7d32d85da948ab558cad40c10b91bde7851d
JamesBond <introbond.dev@gmail.com>
2023-04-22 20:06:36 +0700

    feature: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./pages/global/Topbar";
+import Sidebar from "./pages/global/Sidebar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 import Room from "./components/Room";
 
 function App() {
+    const [theme, colorMode] = useMode();
+    const [isSidebar, setIsSidebar] = useState(true);
+
     return (
-        <div>
-            <Room />
-        </div>
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <Sidebar isSidebar={isSidebar} />
+                    <main className="content">
+                        <Topbar setIsSidebar={setIsSidebar} />
+                        <Routes>
+                            <Route path="/" element={<Room />} />
+                        </Routes>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     )
 }
 
 export default App
+
