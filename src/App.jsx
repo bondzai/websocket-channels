6d636578adc3b6ad88dfeb8162e99640d7b42104
JamesBond <introbond.dev@gmail.com>
2023-04-22 17:48:48 +0700

    deploy on render

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Room from "./components/Room";
 function App() {
     return (
         <div>
-            <h1> hello </h1>
             <Room />
         </div>
     )
--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -57,8 +57,6 @@ const Room = () => {
         }
     };
 
-
-
     const handleClearClick = () => {
         setMessages([]);
     };
